1d22afa20f739e6440c8d5a077258cb7e8af9e43
etude-ist <tyburn@op.pl>
2015-08-13 23:03:12 +0200

    EXERCISES 26-28 added

--- my-answers/src/scala/datastructures/Tree.scala
+++ my-answers/src/scala/datastructures/Tree.scala
@@ -10,4 +10,25 @@ object Tree {
     case Branch(l, r) => 1 + size(l) + size(r)
   }
 
+  // EXERCISE 26: Write a function maximum that returns the maximum element
+  // in a Tree[Int].
+  def maximum(tree: Tree[Int]): Int = tree match {
+    case Leaf(v) => v
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+  // EXERCISE 27: Write a function depth that returns the maximum path length
+  // from the root of a tree to any leaf.
+  def depth[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
+
+  // EXERCISE 28: Write a function map, analogous to the method of the same name
+  // on List, that modifies each element in a tree with a given function
+  def mapTree[A, B](tree: Tree[A])(fn: A => B): Tree[B] = tree match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(mapTree(l)(f), mapTree(r)(f))
+  }
+
 }
